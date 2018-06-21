c3847328cd4d12ee55244879b3e9894f3d1d2f74
Lauro <Lauro199471@gmail.com>
2018-06-20 20:35:13 -0700

    Updating

--- project_2_ALU/project_2_ALU.srcs/sources_1/new/ALU.vhd
+++ project_2_ALU/project_2_ALU.srcs/sources_1/new/ALU.vhd
@@ -2,11 +2,18 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity ALU is
---  Port ( );
+  Port 
+  ( 
+    A, B     : in  STD_LOGIC_VECTOR(7 downto 0);  -- 2 inputs 8-bit
+    ALU_Sel  : in  STD_LOGIC_VECTOR(3 downto 0);  -- 1 input 4-bit for selecting function
+    ALU_Out   : out  STD_LOGIC_VECTOR(7 downto 0); -- 1 output 8-bit 
+    Carryout : out std_logic        -- Carryout flag
+  );
 end ALU;
 
 architecture Behavioral of ALU is
-
+    signal ALU_Result : std_logic_vector (7 downto 0);
+    signal tmp: std_logic_vector (8 downto 0);
 begin
 
 
